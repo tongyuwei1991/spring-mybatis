dd8c80ff34dc75e3ed36bf27bb05101d8e055c82
Administrator <Administrator@192.168.191.7>
2017-04-12 17:41:03 +0800

    1.1

--- spring-mybatis/src/test/java/org/framestudy/spring_mybatis/TestUserService.java
+++ spring-mybatis/src/test/java/org/framestudy/spring_mybatis/TestUserService.java
@@ -19,7 +19,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class TestUserService {
 	
 	@Resource
-	private IUserService userServiceImpl;
+	private IUserService userServiceImpl; 
 	
 	@Ignore
 	public void testSaveUserInfo(){
